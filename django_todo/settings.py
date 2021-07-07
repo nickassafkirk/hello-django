7ec34200dbbe58c1e348a9639ea862d30d28087f
nickassafkirk <nickassafkirk@gmail.com>
2021-07-07 18:27:25 +0000

    test 4

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ln^)&k%6g$pz%y1010f@%2*#q$uy8zjfea9-cmoic4^w^s*v3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://fwsubcsatuarqc:2d9b89781b029be413da262a3c0789c4e02793d5f287085606e2ca8ce7149f61@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/d87j45krjok6rg')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
